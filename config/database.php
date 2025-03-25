2e894ebb7e6e747cd936cf5b7edb1c1a80877349
itsnikhil28 <itsnikhil28@gmail.com>
2025-03-25 15:38:29 +0530

    Changes : Database Migration

--- config/database.php
+++ config/database.php
@@ -31,10 +31,17 @@ return [
 
     'connections' => [
 
+        // 'mongodb' => [
+        //     'driver' => 'mongodb',
+        //     'dsn' => env('DB_URI'),
+        //     'database' => env('DB_DATABASE', 'laravel'),
+        // ],
+
         'mongodb' => [
             'driver' => 'mongodb',
-            'dsn' => env('DB_URI'),
-            'database' => env('DB_DATABASE', 'laravel'),
+            'dsn' => env('MONGODB_URI'),
+            'port' => 27017,
+            'database' => env('MONGODB_DATABASE'),
         ],
 
         'sqlite' => [
